86c06f20f76daf40df6e573b643e5db550a90ce3
NadaAlaa1 <nalaa839@gmail.com>
2024-12-17 21:13:44 +0200

    add counter state & cubit

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,15 @@ class _PointsCounterState extends State<PointsCounter> {
   @override
   void initState() {
     super.initState();
-    // Set initial values for both teams' points to 0
     teamAController.text = '0';
     teamBController.text = '0';
   }
 
   @override
   Widget build(BuildContext context) {
-    // Use MediaQuery to get screen width and height
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    // Responsive text and button sizes
     final double textSize = screenWidth * 0.08;
     final double buttonFontSize = screenWidth * 0.045;
     final double buttonWidth = screenWidth * 0.4;
@@ -48,158 +45,129 @@ class _PointsCounterState extends State<PointsCounter> {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
+        body: Center(
+          child: SizedBox(
+            height: screenHeight * 0.8, // Constrain the overall height
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Wrap the first column with Expanded
                 Expanded(
-                  child: Column(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'Team A',
-                        style: TextStyle(fontSize: textSize),
+                      _buildTeamColumn(
+                          "Team A",
+                          teamAController,
+                          textSize,
+                          buttonFontSize,
+                          buttonWidth,
+                          buttonHeight,
+                          'A'),
+                      Container(
+                        width: 1,
+                        color: Colors.grey,
+                        height: screenHeight * 0.6,
                       ),
-                      // Using AutoSizeTextField for teamAPoints
-                      AutoSizeTextField(
-                        controller: teamAController,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 150),
-                        maxLines: 1,
-                        minFontSize: 50, // Adjust this value as per your preference
-                        onChanged: (value) {
-                          setState(() {
-                            // Parse the value and update the points
-                            teamAPoints = int.tryParse(value) ?? 0;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                      _buildPointButton(
-                        "Add 1 Point",
-                        1,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'A',
-                      ),
-                      SizedBox(height: screenHeight * 0.02),
-                      _buildPointButton(
-                        "Add 2 Points",
-                        2,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'A',
-                      ),
-                      SizedBox(height: screenHeight * 0.02),
-                      _buildPointButton(
-                        "Add 3 Points",
-                        3,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'A',
-                      ),
+                      _buildTeamColumn(
+                          "Team B",
+                          teamBController,
+                          textSize,
+                          buttonFontSize,
+                          buttonWidth,
+                          buttonHeight,
+                          'B'),
                     ],
                   ),
                 ),
-                // Add a divider with flexible height
-                Container(
-                  height: screenHeight * 0.4,
-                  width: 1,
-                  color: Colors.grey,
-                ),
-                // Wrap the second column with Expanded
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(
-                        'Team B',
-                        style: TextStyle(fontSize: textSize),
-                      ),
-                      // Using AutoSizeTextField for teamBPoints
-                      AutoSizeTextField(
-                        controller: teamBController,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 150),
-                        maxLines: 1,
-                        minFontSize: 50, // Adjust this value as per your preference
-                        onChanged: (value) {
-                          setState(() {
-                            // Parse the value and update the points
-                            teamBPoints = int.tryParse(value) ?? 0;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                      _buildPointButton(
-                        "Add 1 Point",
-                        1,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'B',
-                      ),
-                      SizedBox(height: screenHeight * 0.02),
-                      _buildPointButton(
-                        "Add 2 Points",
-                        2,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'B',
-                      ),
-                      SizedBox(height: screenHeight * 0.02),
-                      _buildPointButton(
-                        "Add 3 Points",
-                        3,
-                        buttonFontSize,
-                        buttonWidth,
-                        buttonHeight,
-                        team: 'B',
-                      ),
-                    ],
+                SizedBox(height: screenHeight * 0.05),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: WidgetStatePropertyAll(
+                        EdgeInsets.all(screenWidth * 0.02)),
+                    backgroundColor:
+                        const WidgetStatePropertyAll(Colors.orange),
+                    minimumSize:
+                        WidgetStatePropertyAll(Size(buttonWidth, buttonHeight)),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      teamAPoints = 0;
+                      teamBPoints = 0;
+                      teamAController.text = '0';
+                      teamBController.text = '0';
+                    });
+                  },
+                  child: Text(
+                    'Reset',
+                    style: TextStyle(
+                        fontSize: buttonFontSize, color: Colors.black),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: screenHeight * 0.1),
-            ElevatedButton(
-              style: ButtonStyle(
-                padding: WidgetStatePropertyAll(EdgeInsets.all(screenWidth * 0.02)),
-                backgroundColor: const WidgetStatePropertyAll(Colors.orange),
-                minimumSize: WidgetStatePropertyAll(Size(buttonWidth, buttonHeight)),
-              ),
-              onPressed: () {
-                setState(() {
-                  teamAPoints = 0;
-                  teamBPoints = 0;
-                  teamAController.text = '0';
-                  teamBController.text = '0';
-                });
-              },
-              child: Text('Reset',
-                  style: TextStyle(fontSize: buttonFontSize, color: Colors.black)),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 
-  // Helper method for creating buttons
+  // Helper method for creating team columns
+  Widget _buildTeamColumn(
+      String title,
+      TextEditingController controller,
+      double textSize,
+      double buttonFontSize,
+      double buttonWidth,
+      double buttonHeight,
+      String team) {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // Shrink to fit content
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: textSize),
+          ),
+          SizedBox(height: 10),
+          AutoSizeTextField(
+            controller: controller,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 100),
+            maxLines: 1,
+            minFontSize: 40,
+            onChanged: (value) {
+              setState(() {
+                if (team == 'A') {
+                  teamAPoints = int.tryParse(value) ?? 0;
+                } else {
+                  teamBPoints = int.tryParse(value) ?? 0;
+                }
+              });
+            },
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+            ),
+          ),
+          SizedBox(height: 10),
+          _buildPointButton(
+              "Add 1 Point", 1, buttonFontSize, buttonWidth, buttonHeight, team),
+          SizedBox(height: 10),
+          _buildPointButton(
+              "Add 2 Points", 2, buttonFontSize, buttonWidth, buttonHeight, team),
+          SizedBox(height: 10),
+          _buildPointButton(
+              "Add 3 Points", 3, buttonFontSize, buttonWidth, buttonHeight, team),
+        ],
+      ),
+    );
+  }
+
   ElevatedButton _buildPointButton(
       String text, int points, double fontSize, double width, double height,
-      {required String team}) {
+      String team) {
     return ElevatedButton(
       style: ButtonStyle(
-        padding: const WidgetStatePropertyAll(EdgeInsets.all(8)),
         backgroundColor: const WidgetStatePropertyAll(Colors.orange),
         minimumSize: WidgetStatePropertyAll(Size(width, height)),
       ),
